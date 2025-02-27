cb9d1bf54edc5bdf4d8512f04a33e620a6e3e2fd
Naveen Kumar <70622189+Ns-AnoNymouS@users.noreply.github.com>
2025-02-27 09:20:46 +0530

    Update carouselData.js

--- src/components/ProShows/data/carouselData.js
+++ src/components/ProShows/data/carouselData.js
@@ -1,15 +1,4 @@
 export const carouselData = [
-  // {
-  //   id: 1,
-  //   image: ["/assets/proshows/proshow3.webp"], // Relative path from public
-  //   author: "ABHISARGA",
-  //   date: "2025",
-  //   title: "DJ Gandhi Vijay",
-  //   topic: "DJ NIGHT",
-  //   link: "https://www.instagram.com/stories/highlights/18049394729292508/",
-  //   description:
-  //   "Get ready to groove with DJ Gandhi Vijay, a renowned DJ known for his electrifying performances. Having played at numerous high-profile promotional shows, including the famous Pushpa 2 event, he is set to bring an unforgettable DJ night to our annual fest!",
-  // },
   {
     id: 1,
     image: [
@@ -38,4 +27,15 @@ export const carouselData = [
     description:
       "Having performed at prestigious institutions like IIT Madras and NIT Surathkal, they are set to deliver an electrifying night filled with incredible music and unforgettable vibes!",
   },
+  {
+     id: 3,
+     image: ["/assets/proshows/proshow3.webp"], // Relative path from public
+     author: "ABHISARGA",
+     date: "2025",
+     title: "DJ Gandhi Vijay",
+     topic: "DJ NIGHT",
+     link: "https://www.instagram.com/stories/highlights/18049394729292508/",
+     description:
+      "Get ready to groove with DJ Gandhi Vijay, a renowned DJ known for his electrifying performances. Having played at numerous high-profile promotional shows, including the famous Pushpa 2 event, he is set to bring an unforgettable DJ night to our annual fest!",
+   },
 ];
